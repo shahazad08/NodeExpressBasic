f87a7a9f22ef8f98647b8b12e1cc5122b19da534
shahazad08 <sk.shahazad@gmail.com>
2021-09-27 12:56:02 +0530

    Added:Creating the HTML Pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
+const path=require('path')
 const express=require('express')
 const router=express.Router();
 
 router.get('/', (req, res, next) => {
     console.log("This always Runs")
-    res.send('<h1>Hello From Express JS</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+   // res.send('<h1>Hello From Express JS</h1>')
+
 })
 
 module.exports=router;
