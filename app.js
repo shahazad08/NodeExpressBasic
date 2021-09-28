3b7179cbfbbed93ec45a60d21fd310afcd2c5050
shahazad08 <sk.shahazad@gmail.com>
2021-09-28 12:40:45 +0530

    Added: Page 404, root path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Welcome to Express JS");
 
 const bodyParser = require('body-parser');
 const express=require('express')
+const path=require('path')
 const app=express();
 
 const adminRoutes=require('./routes/admin')
@@ -13,7 +14,8 @@ app.use(shopRoutes)
 app.use('/admin',adminRoutes)
 
 app.use((req,res,next)=> {
-    res.status(404).send('<h1>Page Not Found</h1>')
+//    res.status(404).send('<h1>Page Not Found</h1>')
+      res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(4001)
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const path=require('path')
 const express=require('express')
 const router=express.Router();
+const rootDir=require('../util.js/path');
 
 router.get('/', (req, res, next) => {
     console.log("This always Runs")
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
    // res.send('<h1>Hello From Express JS</h1>')
 
 })
