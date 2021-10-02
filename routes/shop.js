5e87e72ba562f6f1e18c9ffcd31744bcb4fc95ef
shahazad08 <sk.shahazad@gmail.com>
2021-10-02 13:28:18 +0530

    Added:Templete engine added Dynamic in shop.pug & modify css

--- routes/shop.js
+++ routes/shop.js
@@ -7,10 +7,15 @@ const adminData=require('./admin')
 router.get('/', (req, res, next) => {
     console.log("This always Runs Shop Page")
     console.log('Shop JS FIle',adminData.products)
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))    //Rendering HTML FIle
-    res.render('shop')    // Rendering Pug FIle
-   // res.send('<h1>Hello From Express JS</h1>')   // Rendering Simple HTML FIle
+    const products=adminData.products
+    res.render('shop', {prods:products,docTitle:'Shop'})    // Rendering Pug FIle
+
+
 
 })
 
 module.exports=router;
+
+
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))    //Rendering HTML FIle
+    // res.send('<h1>Hello From Express JS</h1>')   // Rendering Simple HTML FIle
