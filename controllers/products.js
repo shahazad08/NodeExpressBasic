2d03a5429710acd49308df0e6761ac3b6500ab1f
shahazad08 <sk.shahazad@gmail.com>
2021-10-19 12:47:13 +0530

    Added: Models Product added

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,4 @@
-const products=[]
-
+const Product= require('../models/product')
 exports.getAddProduct=(req, res, next) => {
     console.log("Add Product Pagee");
     res.render('add-product', {pageTitle:'Add Product', 
@@ -10,12 +9,13 @@ exports.getAddProduct=(req, res, next) => {
 }
 
 exports.postAddProduct=(req, res, next) => {
-    console.log(req.body)
-    products.push({title:req.body.title})
+    const product=new Product(req.body.title)
+    product.save()
     res.redirect('/');
 }
 
 exports.getProducts=(req, res, next) => {
+    const products=Product.fetchAll()
     res.render('shop', {prods:products,
         pageTitle:'Shop', 
         path: '/', 
