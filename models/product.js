57b8cd6c093cd69ccf1a60e5514edc36b88d35cc
shahazad08 <sk.shahazad@gmail.com>
2021-10-20 14:29:30 +0530

    Added: Models

--- models/product.js
+++ models/product.js
@@ -1,12 +1,22 @@
-const products=[]
+const fs = require('fs')
+const path = require('path')
 
-module.exports=class Product {
+module.exports = class Product {
     constructor(t) {
-        this.title=t
+        this.title = t
     }
 
     save() {
-        products.push(this)
+        const p = path.join
+            (path.dirname(process.mainModule.filename),
+                'data',
+                'products.json'
+            );
+        fs.readFile(p,(err, fileContent)=> {
+            console.log("File Error is",err)
+        })
+
+
     }
 
     static fetchAll() {
