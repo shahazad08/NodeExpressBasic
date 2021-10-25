314bf5b87ac05f1e8893e7822b7a5f8e75ca2003
shahazad08 <sk.shahazad@gmail.com>
2021-10-25 12:27:43 +0530

    Added: Diffferent routes in a controller with Views & Resolved the 404 error

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product= require('../models/product')
+
 exports.getAddProduct=(req, res, next) => {
     console.log("Add Product Pagee");
     res.render('admin/add-product', {pageTitle:'Add Product', 
@@ -14,15 +15,12 @@ exports.postAddProduct=(req, res, next) => {
     res.redirect('/');
 }
 
-exports.getProducts=(req, res, next) => {
+exports.getProducts=(req, res,next)=> {
     Product.fetchAll(products=> {
-        res.render('shop/product-list', {prods:products,
-            pageTitle:'Shop', 
-            path: '/', 
-            hasProducts:products.length > 0,
-            activeShop:true,
-            productCSS:true
+        res.render('admin/products', {
+            prods:products,
+            pageTitle:'Admin Products', 
+            path: '/admin/products', 
         })    
-    })
-
+    })  
 }
