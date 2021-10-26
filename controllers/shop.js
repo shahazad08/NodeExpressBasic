641371dce7ddca7ddbcd918a48da0f2cd7d9a156
shahazad08 <sk.shahazad@gmail.com>
2021-10-26 11:51:17 +0530

    Added: Rendering the Product Detail Vire (product-details.ejs)

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,8 +14,9 @@ exports.getProduct=(req, res, next)=> {
     const prodId=req.params.productId
     Product.findById(prodId, product=> {
         console.log(product)
+        res.render('shop/product-details', {product:product, pageTitle:product.title, path:'/products'})
     })
-    res.redirect('/')
+    
 }
 
 exports.getIndex=(req,res,next)=> {
