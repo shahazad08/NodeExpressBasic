cd935febe2c36e006cdeb65f1ea4aa25bcc6b565
shahazad08 <sk.shahazad@gmail.com>
2021-10-27 18:13:32 +0530

    Added: Edit Product functionality added

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct=(req, res, next) => {
     const imageUrl=req.body.imageUrl;
     const price=req.body.price;
     const description=req.body.description;
-    const product=new Product(title, imageUrl, description, price)
+    const product=new Product(null, title, imageUrl, description, price)
     product.save()
     res.redirect('/');
 }
@@ -26,6 +26,7 @@ exports.getEditProduct=(req, res, next) => {
     }
     const prodId=req.params.productId
     Product.findById(prodId, product=> {
+        console.log(prodId)
         if(!product) {
             return res.redirect('/')
         }
@@ -38,6 +39,19 @@ exports.getEditProduct=(req, res, next) => {
     })
     //console.log("Add Product Pagee");
    
+}
+
+exports.postEditProduct=(req,res,next)=> {
+    const prodId=req.body.productId
+    const updatedTitle=req.body.title
+    const updatedPrice=req.body.price
+    const updatedImageUrl=req.body.imageUrl
+    const updatedDesc=req.body.description
+    const updatedProduct=new Product(prodId, updatedTitle, updatedPrice, updatedImageUrl, updatedDesc)
+    updatedProduct.save()
+    res.redirect('/admin/products')
+    console.log("Updated Product is",prodId)
+
 }
 
 exports.getProducts=(req, res,next)=> {
