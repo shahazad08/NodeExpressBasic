57c8685a4866114ce4e5869ab95a54ae7f0d4473
shahazad08 <sk.shahazad@gmail.com>
2021-11-10 16:57:11 +0530

    Added:Fetching a Single Product in shop & Admin in controller file

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,12 +16,14 @@ exports.getProducts=(req, res, next) => {
 }
 
 exports.getProduct=(req, res, next)=> {
-    
     const prodId=req.params.productId
-    Product.findById(prodId)
-        .then(([product])=> {
-            console.log(product[0])
-            res.render('shop/product-details', {product:product[0], pageTitle:product.title, path:'/products'})
+    Product.findByPk(prodId)
+        .then(product => {
+            console.log(product)
+            res.render('shop/product-details', {
+                product:product, 
+                pageTitle:product.title, 
+                path:'/products'})
         })
         .catch(err=> {
             console.log(err)
